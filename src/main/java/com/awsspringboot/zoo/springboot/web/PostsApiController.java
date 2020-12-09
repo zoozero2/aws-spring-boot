fedda3194792488bb0514be2475dafa6686fb5b5
shkk00 <ksh0810@gmail.com>
2020-12-09 17:01:50 +0900

    PostsApiControllerTest 성공! 111p

--- src/main/java/com/awsspringboot/zoo/springboot/web/PostsApiController.java
+++ src/main/java/com/awsspringboot/zoo/springboot/web/PostsApiController.java
@@ -1,7 +1,7 @@
 package com.awsspringboot.zoo.springboot.web;
 
 import com.awsspringboot.zoo.springboot.service.posts.PostService;
-import com.awsspringboot.zoo.springboot.web.dto.PostSaveRequestDto;
+import com.awsspringboot.zoo.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +14,7 @@ public class PostsApiController {
     private final PostService postService;
 
     @PostMapping("api/v1/posts")
-    public Long save(@RequestBody PostSaveRequestDto requestDto) {
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
 
         return postService.save(requestDto);
     }
