19fcf6247f892549ba663a9def49a70e067a9265
asqw2323 <asqw2323@gmail.com>
2020-12-21 01:21:09 +0900

    delete

--- src/main/java/com/awsspringboot/zoo/springboot/service/posts/PostsService.java
+++ src/main/java/com/awsspringboot/zoo/springboot/service/posts/PostsService.java
@@ -47,4 +47,10 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = "+id));
+        postsRepository.delete(posts);
+    }
 }
