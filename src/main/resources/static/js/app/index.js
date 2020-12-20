867c082fb8b5df896b4f2fc8d11109ad1a287c0d
asqw2323 <asqw2323@gmail.com>
2020-12-21 00:03:29 +0900

    delete

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -47,7 +47,19 @@ var index = {
         })
     },
     delete : {
+var id = $('#id').val();
 
+        $.ajax({
+            type : 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType : 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (errer) {
+            alert(JSON.stringify(errer));
+        })
     }
 };
 index.init();
